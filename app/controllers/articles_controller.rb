76751f88ef9726e31fbe5dcfd0bca30c4b4b81e1
Dennys Lopez Dinza <dldinza.web@gmail.com>
2016-11-08 12:35:47 -0300

    CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,16 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -18,8 +27,23 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def show
+  def update
     @article = Article.find(params[:id])
+
+    respond_to do |format|
+      if @article.update article_params
+        format.html { redirect_to @article }
+      else
+        format.html { render 'edit' }
+      end
+    end
+  end
+
+  def destroy
+    article = Article.find params[:id]
+    article.destroy
+
+    redirect_to articles_path
   end
 
   private
